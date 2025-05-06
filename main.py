4a804858a6f1403c6e96591d281a1319c3f70fd3
GolazoSteve <166519142+GolazoSteve@users.noreply.github.com>
2025-05-06 19:52:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 TRIGGER_KEY = os.getenv("TRIGGER_KEY")
 
-GRAPHQL_URL = "https://bifrost-api.mlb.com/api/graphql
-"
+GRAPHQL_URL = "https://bifrost-api.mlb.com/api/graphql"
 
 # Raw GraphQL query string
 GRAPHQL_QUERY = """
